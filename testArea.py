8afa13ee01cf19068af16edcfa3ce2fbe310b162
SimonXming <774286950@qq.com>
2015-02-25 14:04:01 +0800

    just add to test.

--- testArea.py
+++ testArea.py
@@ -1,2 +1,19 @@
-a = {1,2,3,4}
-print(help(a))
+import random
+class Accumulator(object):
+	"""docstring for Accumulator"""
+
+	def __init__(self,num):
+		super(Accumulator, self).__init__()
+		self.total = 0.0
+		self.N = num
+	def addDataValue(self,value):
+		self.total += value
+
+	def mean(self):
+		return(self.total/self.N)
+
+		
+x = Accumulator(100000)
+for i in range(0,x.N):
+	x.addDataValue(random.random())
+print(x.mean())
