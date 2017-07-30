e71b3071f7d2be42ebf7247c007e151e67d3dc9a
SimonXming <simon_xu@outlook.com>
2017-07-30 23:04:31 +0800

    Update detail.

--- pomotodo/cron_pomotodo/test_post_data.py
+++ pomotodo/cron_pomotodo/test_post_data.py
@@ -6,7 +6,7 @@ from influxdb import InfluxDBClient
 
 TODOS_API = "https://api.pomotodo.com/1/todos"
 HAEDER = {
-    "Authorization": "token crZ1lME6ctYMjACMAtw8N7Fmv8ZowYeM2QFq47wYjot7mmi2Rzo8NHT6bAlzw0K1a82AfSnrb1C4CuTKDpLS85QeuzH42ck9"
+    "Authorization": "token"
 }
 
 
@@ -31,7 +31,7 @@ def get_todos():
             single_data["fields"]["count"] = float(entry["costed_pomo_count"])
             list_data.append(single_data)
 
-    client = InfluxDBClient(host='130.211.243.129', port=8086, database='pomotodo')
+    client = InfluxDBClient(host='localhost', port=8086, database='pomotodo')
     client.write_points(list_data)
 
 
